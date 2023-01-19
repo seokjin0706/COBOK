88eef07e963b4e98d2dade0be6019113dc494c16
youhyeonseokyou <sksj0111@naver.com>
2023-01-19 22:32:34 +0900

    [MODIFY] 상대 경로 수정

--- convergence/core/Data/TradingData.py
+++ convergence/core/Data/TradingData.py
@@ -1,7 +1,7 @@
 import pandas as pd
-
+import os
 def getTradingData():
-    data = pd.read_csv("Data/TradingData.csv").drop(['Unnamed: 0'],axis = 1)
+    data = pd.read_csv(os.path.dirname(__file__) + "/TradingData.csv").drop(['Unnamed: 0'],axis = 1)
     result = {}
     for i in data.columns:
         result[i] = str(data[i].iloc[0])
