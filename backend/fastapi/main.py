f9fe1fec75c941f78701ae8e78fc380e68e26a3a
seokjin0706 <seokjin_0706@naver.com>
2023-02-16 03:58:19 +0900

    [MODIFY] 로컬에서 서버간 api 요청 가능하도록 CORS 설정

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -4,9 +4,21 @@ sys.path.append(os.path.dirname(__file__) + "/../../convergence/core")
 from AutoTrading import trade
 from Data.TradingData import getTradingData
 app = FastAPI()
-
 from pydantic import BaseModel
 
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost:3001",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class User(BaseModel):
     userName : str
     apiKey : str
