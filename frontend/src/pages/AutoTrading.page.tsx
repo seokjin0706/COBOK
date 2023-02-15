4100486399032bec9dde4e6d684aa53f713ebe7b
seokjin0706 <seokjin_0706@naver.com>
2023-02-16 04:48:32 +0900

    [ADD] 코인 자동 거래 API 요청 구현

--- frontend/src/pages/AutoTrading.page.tsx
+++ frontend/src/pages/AutoTrading.page.tsx
@@ -1,27 +1,43 @@
-import { Button, Card } from 'react-bootstrap';
-import { AutoTradingResult } from './../components/AutoTrading.component';
-
+import { Button, Card } from "react-bootstrap";
+import { AutoTradingResult } from "./../components/AutoTrading.component";
+import axios, { AxiosError } from "axios";
 export function AutoTrading() {
-    return (
-        <div>
-            <div className="center">
-                <Card className='AutoTrading' bg="light">
-                    <Card.Body>
-                        <Card.Title style={{ textAlign: 'center', fontSize: '35px' }}>AutoTraing</Card.Title>
+  return (
+    <div>
+      <div className="center">
+        <Card className="AutoTrading" bg="light">
+          <Card.Body>
+            <Card.Title style={{ textAlign: "center", fontSize: "35px" }}>
+              AutoTraing
+            </Card.Title>
 
-                        <Card.Text>
-                            <Button className='login-btn' variant="light" type="submit">
-                                코인 자동매매 시작
-                            </Button>
-                            <Button className='login-btn' variant="light" type="submit">
-                                코인 자동매매 종료
-                            </Button>
-                            <AutoTradingResult />
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
-        </div>
-
-    );
+            <Card.Text>
+              <Button
+                className="login-btn"
+                variant="light"
+                type="button"
+                onClick={() => {
+                  axios
+                    .post("http://localhost:8000/api/btc/auto-trading", {
+                      userName: "seokjin2",
+                      apiKey:
+                        "yqrURCKivzwjsTyzxs16JIotlcVVUbHKq71uQQcqIYACzeMwU65BY3HDgqnB2ijL",
+                      apiSecretKey:
+                        "gyfUDaf559JJA8qmqQE8ZK3pOUd7vad26ZUEYOalpUVy5ScBBWnJOFNZSkyeUMjF",
+                    })
+                    .then((result) => {});
+                }}
+              >
+                코인 자동매매 시작
+              </Button>
+              <Button className="login-btn" variant="light" type="submit">
+                코인 자동매매 종료
+              </Button>
+              <AutoTradingResult />
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 }
